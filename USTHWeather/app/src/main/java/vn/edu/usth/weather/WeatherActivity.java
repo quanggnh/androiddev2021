8073bc48d99a5f11e01cf698fbf8271557c19f44
quanghaopro <megamanzeroandciel2000@gmail.com>
2020-12-13 21:50:27 +0700

    #1 Labwork

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,56 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("Created","onCreate");
+
+//        TextView mTextView = (TextView) findViewById(R.id.text_message);
+//        mTextView.setText("Hello world");
+        ForecastFragment firstFrag = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container,firstFrag,null).commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Stopped","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Destroyed","onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Paused","onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Resumed","onResume");      }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Started","onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Restarted","onRestart");
+    }
+}
